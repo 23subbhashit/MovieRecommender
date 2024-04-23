b7e820aab82e494dd5ea08c6c45acb56be0eeab0
Subbhashit Mukherjee <43717493+23subbhashit@users.noreply.github.com>
2024-04-24 02:38:28 +0530

    Update settings.py

--- mrs/settings.py
+++ mrs/settings.py
@@ -26,7 +26,7 @@ CORS_ALLOWED_ORIGINS = [
 ]
 CSRF_TRUSTED_ORIGINS =  [
     "https://movierecommender-he6k.onrender.com",
-    "movierecommender-he6k.onrender.com"
+    
     # Add other allowed origins as needed
 ]
 # Quick-start development settings - unsuitable for production
